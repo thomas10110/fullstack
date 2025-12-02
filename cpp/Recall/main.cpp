925f27ffa0573b89ccd0d86dc91e36546bd186c2
Tomas <mistuna.tomas@gmail.com>
2025-12-02 21:26:52 +0100

    Refactor code structure for improved readability and maintainability

---
+++ cpp/Recall/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    double x {};
+    double y {};
+
+    std::cout << "Enter first number:"<<std::endl;
+    std::cin >> x;
+    x = x*y;
+    std::cout << "Your number is:" << x;
+
+}
